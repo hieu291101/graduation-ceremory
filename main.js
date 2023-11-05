3444640f5d9d754fb60f6124e8c720bb6b6d6112
hieu291101 <latrunghieuvt1@gmail.com>
2023-11-05 22:18:17 +0700

    5/11 - commit to main

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    window.alert("Hãy truy cập bằng máy tính để có thể trải nghiệm tốt hơn nha\nNhớ nhấn OPEN nữaaaaaa");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByClassName('open')[0].addEventListener('click', function () {
+      document.getElementsByClassName('card-packaging')[0].classList.add('is-open')
+    })
+  
+    document.getElementsByClassName('close')[0].addEventListener('click', function () {
+      document.getElementsByClassName('card-packaging')[0].classList.remove('is-open')
+    })
+  });
+
